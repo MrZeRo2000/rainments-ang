cafc63d5ab982125c99d5c6fcbfab73af7ff7abd
MrZeRo2000 <MrZeRo2000@mail.ru>
2020-01-06 20:20:20 +0200

    removed unneeded tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('rainments-ang');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('rainments-ang app is running!');
-  });
 });
