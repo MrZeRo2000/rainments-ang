262119b8c24d5fb78337880c50ef54d26cf41679
MrZeRo2000 <MrZeRo2000@mail.ru>
2020-01-17 17:37:45 +0200

    created and added model

---
+++ src/app/model/payment-objects.spec.ts
@@ -0,0 +1,7 @@
+import { PaymentObjects } from './payment-objects';
+
+describe('PaymentObjects', () => {
+  it('should create an instance', () => {
+    expect(new PaymentObjects()).toBeTruthy();
+  });
+});
---
+++ src/app/model/payment-objects.ts
@@ -0,0 +1,4 @@
+export class PaymentObjects {
+
+  constructor(public id?: number, public name?: string) {}
+}
