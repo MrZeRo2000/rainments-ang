4f2f31d63452bbfb301fb97d828f1777e2470942
MrZeRo2000 <MrZeRo2000@mail.ru>
2020-05-01 20:41:57 +0300

    fixed tests for PaymentObjectTotalsRepository

--- src/app/repository/payment-object-totals-repository.spec.ts
+++ src/app/repository/payment-object-totals-repository.spec.ts
@@ -1,7 +1,21 @@
 import { PaymentObjectTotalsRepository } from './payment-object-totals-repository';
+import {TestBed} from '@angular/core/testing';
+import {RestDataSource} from '../data-source/rest-data-source';
+import {MessagesService} from '../messages/messages.service';
+import {RestUrlEnv} from '../config/configuration';
+import {PaymentObjectPersistRepository} from './payment-object-persist-repository';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('PaymentObjectTotalsRepository', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [RestDataSource, RestUrlEnv],
+    imports: [HttpClientTestingModule]
+  }));
+
   it('should create an instance', () => {
-    expect(new PaymentObjectTotalsRepository()).toBeTruthy();
+    expect(new PaymentObjectTotalsRepository(
+      TestBed.inject(RestDataSource),
+      TestBed.inject(MessagesService)
+    )).toBeTruthy();
   });
 });
