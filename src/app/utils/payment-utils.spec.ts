8f875fe557765204c6dc90ef81180b9fb9d3f4db
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-04-03 19:41:36 +0300

    implemented groupBy method for payments

--- src/app/utils/payment-utils.spec.ts
+++ src/app/utils/payment-utils.spec.ts
@@ -21,16 +21,42 @@ describe('PaymentUtils', () => {
   const testPayments: Array<Payment> = [
     new Payment(1, date1, date1, paymentObject1, paymentGroup1, product1, 0, 12.5, 0.3),
     new Payment(2, date1, date1, paymentObject1, paymentGroup1, product2, 0, 2.5, 0.1),
-    new Payment(3, date1, date1, paymentObject1, paymentGroup2, product1, 0, 3.1, 0.2),
-    new Payment(4, date1, date1, paymentObject1, paymentGroup2, product2, 0, 7.8, 0.24)
+    new Payment(3, date2, date2, paymentObject1, paymentGroup2, product1, 0, 3.1, 0.2),
+    new Payment(4, date2, date2, paymentObject1, paymentGroup2, product2, 0, 7.8, 0.24)
   ];
+  const totals = testPayments.reduce(
+    (a, v) => {
+      a[0] = a[0] + v.paymentAmount; a[1] = a[1] + v.commissionAmount; return a;
+      }, [0, 0]
+  );
+
+  it('totals', () => {
+    expect(totals[0]).toBeCloseTo(12.5+2.5+3.1+7.8);
+    expect(totals[1]).toBeCloseTo(0.3+0.1+0.2+0.24);
+  });
 
   it('calcPaymentAmountSummary', () => {
     const summary: PaymentAmountSummary = PaymentUtils.calcPaymentAmountSummary(testPayments);
-    expect(summary.paymentAmount).toBeCloseTo(12.5+2.5+3.1+7.8);
-    expect(summary.commissionAmount).toBeCloseTo(0.3+0.1+0.2+0.24);
+    expect(summary.paymentAmount).toBeCloseTo(totals[0]);
+    expect(summary.commissionAmount).toBeCloseTo(totals[1]);
   });
 
   const groups = ['periodDate', 'paymentObject', 'paymentGroup', 'product'];
+
+  const groupKeys1 = ['periodDate', 'paymentObject'];
+  it ('groupBy ' + JSON.stringify(groupKeys1), () => {
+    const result = PaymentUtils.groupBy(testPayments, groupKeys1);
+    const totalPaymentAmount = result.reduce((a, v) => a + v.paymentAmount, 0);
+    const totalCommissionAmount = result.reduce((a, v) => a + v.commissionAmount, 0);
+    expect(totalPaymentAmount).toBeCloseTo(totals[0]);
+    expect(totalCommissionAmount).toBeCloseTo(totals[1]);
+    expect(result.length).toBe(2);
+    expect(result[0].paymentGroup).toBeUndefined();
+    expect(result[0].product).toBeUndefined();
+    expect(result[0].periodDate).toEqual(date1);
+    expect(result[1].periodDate).toEqual(date2);
+    expect(JSON.stringify(result[0].paymentObject)).toBe(JSON.stringify(paymentObject1));
+    expect(JSON.stringify(result[1].paymentObject)).toBe(JSON.stringify(paymentObject1));
+  });
 
 });
--- src/app/utils/payment-utils.ts
+++ src/app/utils/payment-utils.ts
@@ -8,4 +8,35 @@ export class PaymentUtils {
       (a, v) => a.addAmounts(v.paymentAmount, v.commissionAmount), new PaymentAmountSummary(0, 0)
     );
   }
+
+  static groupBy(payments: Array<Payment>, groupFields: Array<string>): Array<Payment> {
+    const groupedItems = payments.reduce(
+      (ap, vp) => {
+        const groupObject = Object.keys(vp)
+          .filter(v => groupFields.includes(v))
+          .reduce((a, v) => {a[v] = vp[v]; return a;},
+            {});
+
+        const groupKey = JSON.stringify(groupObject);
+        if (ap[groupKey]) {
+          ap[groupKey] = [vp.paymentAmount + ap[groupKey][0], vp.commissionAmount + ap[groupKey][1]];
+        } else {
+          ap[groupKey] = [vp.paymentAmount, vp.commissionAmount];
+        }
+
+        return ap;
+      },
+      {}
+    );
+
+    return Object.keys(groupedItems).reduce((a, v) => {
+      const payment: Payment = JSON.parse(v);
+      payment.date = new Date(payment.date);
+      payment.periodDate = new Date(payment.periodDate);
+      payment.paymentAmount = groupedItems[v][0];
+      payment.commissionAmount = groupedItems[v][1];
+      a.push(payment);
+      return a;
+    },[]);
+  }
 }
