724c11a645d4f99d1f08245e68146714b0b9c151
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-10-05 17:10:40 +0300

    iss108 Copy/Paste: adjust decimal separators, if decimal separator is comma (,) the number appears without any separators

--- src/app/core/utils/clipboard-number-converter.ts
+++ src/app/core/utils/clipboard-number-converter.ts
@@ -3,7 +3,7 @@ export class ClipboardNumberConverter {
   public static getConverted(event: any): number {
     const clipboardText = event.clipboardData?.getData('text');
     if (!!clipboardText) {
-      const convertedText = clipboardText.replace(',', '.');
+      const convertedText = clipboardText.trim().replace(',', '.');
       return parseFloat(convertedText);
     } else {
       return undefined;
