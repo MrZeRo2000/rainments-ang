9a61c81fb05113e43c9daae9738608916613a27c
MrZeRo2000 <MrZeRo2000@mail.ru>
2022-02-22 12:56:09 +0200

    iss114 Upgrade ngx-bootstrap to 8* stable version (as soon as it is available) supporting Angular 13, fixed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,18 +4,26 @@ import { AppComponent } from './app.component';
 import { NavTopComponent } from './components/nav-top/nav-top.component';
 import {MessageComponent} from './messages/message.component';
 import {AlertModule} from 'ngx-bootstrap/alert';
+import {AppInfoComponent} from './components/app-info/app-info.component';
+import {RepositoryModule} from './repository/repository.module';
+import {RestUrlEnv} from './config/configuration';
+import {RestDataSource} from './data-source/rest-data-source';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule, AlertModule.forRoot()
+        RouterTestingModule,
+        AlertModule.forRoot(),
+        RepositoryModule
       ],
       declarations: [
         AppComponent,
+        AppInfoComponent,
         NavTopComponent,
         MessageComponent
       ],
+      providers: [RestUrlEnv, RestDataSource]
     }).compileComponents();
   });
 
