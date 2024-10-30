7da816a648f60a782f1c47919a376d1a21884173
MrZeRo2000 <MrZeRo2020@outlook.com>
2024-10-30 14:32:01 +0200

    Fixed tests after migration to Angular 18

--- src/app/components/backup-database-button/backup-database-button.component.spec.ts
+++ src/app/components/backup-database-button/backup-database-button.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BackupDatabaseButtonComponent } from './backup-database-button.component';
+import {RepositoryModule} from "../../repository/repository.module";
+import {RestUrlEnv} from "../../config/configuration";
+import {RestDataSource} from "../../data-source/rest-data-source";
+import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {provideHttpClientTesting} from "@angular/common/http/testing";
+import {FontAwesomeIconsModule} from "../../font-awesome-icons/font-awesome-icons.module";
 
 describe('BackupDatabaseButtonComponent', () => {
   let component: BackupDatabaseButtonComponent;
@@ -8,7 +14,9 @@ describe('BackupDatabaseButtonComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [BackupDatabaseButtonComponent]
+      declarations: [BackupDatabaseButtonComponent],
+      imports: [RepositoryModule, FontAwesomeIconsModule],
+      providers: [RestUrlEnv, RestDataSource, provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
     });
     fixture = TestBed.createComponent(BackupDatabaseButtonComponent);
     component = fixture.componentInstance;
