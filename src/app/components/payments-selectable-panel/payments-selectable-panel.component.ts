055ccbe83a7a17a95e97bff2bfdace43eba718bb
MrZeRo2000 <MrZeRo2020@outlook.com>
2026-07-01 09:37:03 +0300

    iss#186 Migrate PaymentsSelectablePanelComponent

--- src/app/components/payments-selectable-panel/payments-selectable-panel.component.ts
+++ src/app/components/payments-selectable-panel/payments-selectable-panel.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import {CoreSelectablePanelComponent} from '../../core/components/core-selectable-panel/core-selectable-panel.component';
 import {Payment} from '../../model/payment';
-import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-payments-selectable-panel',
   templateUrl: './payments-selectable-panel.component.html',
   imports: [
-    FaIconComponent
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   styleUrls: ['./payments-selectable-panel.component.scss']
 })
